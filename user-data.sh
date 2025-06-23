58d972fc71024fa1044a1774f6f1764413400e16
Muhammad Nazir Said <109415048+M-NazirSaid@users.noreply.github.com>
2025-06-23 18:16:20 +0300

    Delete user-data.sh

--- user-data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo yum update -y
-sudo yum install httpd -y
-sudo systemctl start httpd
-sudo systemctl enable httpd
-echo "<h1>Welcome to Scalable Web App</h1>" | sudo tee /var/www/html/index.html
